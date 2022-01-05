b0329cba97df9c907a02f0da4abb220e3f056186
kauepalota <kurpfy@gmail.com>
2022-01-05 17:00:41 -0300

    feat: add hikaricp

--- src/main/java/com/harmonyplugins/dbutils/sql/SQLDatabase.java
+++ src/main/java/com/harmonyplugins/dbutils/sql/SQLDatabase.java
@@ -1,21 +1,44 @@
 package com.harmonyplugins.dbutils.sql;
 
-import be.bendem.sqlstreams.Sql;
 import com.harmonyplugins.dbutils.model.Database;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SQLDatabase implements Database<Connection> {
+    private static final AtomicInteger POOL_COUNTER = new AtomicInteger(0);
+
+    private static final int MAXIMUM_POOL_SIZE = (Runtime.getRuntime().availableProcessors() * 2) + 1;
+    private static final int MINIMUM_IDLE = Math.min(MAXIMUM_POOL_SIZE, 10);
+
+    private static final long MAX_LIFETIME = TimeUnit.MINUTES.toMillis(30);
+    private static final long CONNECTION_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
+    private static final long LEAK_DETECTION_THRESHOLD = TimeUnit.SECONDS.toMillis(10);
+
     private static final String JDBC_URL = "jdbc:%s://%s:%s/%s";
 
-    private Connection connection;
+    private HikariDataSource source;
+
+    public HikariDataSource getSource() {
+        return source;
+    }
 
     @Override
     public Connection getConnection() {
-        return this.connection;
+        try {
+            return source.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
@@ -27,39 +50,61 @@ public class SQLDatabase implements Database<Connection> {
             properties.getProperty("database")
         );
 
-        try {
-            this.connection = DriverManager.getConnection(
-                url,
-                properties.getProperty("username"),
-                properties.getProperty("password")
-            );
+        final HikariConfig hikari = new HikariConfig();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        hikari.setPoolName("helper-sql-" + POOL_COUNTER.getAndIncrement());
+
+        hikari.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        hikari.setJdbcUrl(url);
+
+        hikari.setUsername(properties.getProperty("username"));
+        hikari.setPassword(properties.getProperty("password"));
+
+        hikari.setMaximumPoolSize(MAXIMUM_POOL_SIZE);
+        hikari.setMinimumIdle(MINIMUM_IDLE);
+
+        hikari.setMaxLifetime(MAX_LIFETIME);
+        hikari.setConnectionTimeout(CONNECTION_TIMEOUT);
+        hikari.setLeakDetectionThreshold(LEAK_DETECTION_THRESHOLD);
+
+        Map<String, String> map = new HashMap<>() {{
+            put("useUnicode", "true");
+            put("characterEncoding", "utf8");
+
+            put("cachePrepStmts", "true");
+            put("prepStmtCacheSize", "250");
+            put("prepStmtCacheSqlLimit", "2048");
+            put("useServerPrepStmts", "true");
+            put("useLocalSessionState", "true");
+            put("rewriteBatchedStatements", "true");
+            put("cacheResultSetMetadata", "true");
+            put("cacheServerConfiguration", "true");
+            put("elideSetAutoCommits", "true");
+            put("maintainTimeStats", "false");
+            put("alwaysSendSetIsolation", "false");
+            put("cacheCallableStmts", "true");
+
+            put("socketTimeout", String.valueOf(TimeUnit.SECONDS.toMillis(30)));
+        }};
+
+        for (Map.Entry<String, String> property : map.entrySet()) {
+            hikari.addDataSourceProperty(property.getKey(), property.getValue());
         }
+
+        this.source = new HikariDataSource(hikari);
     }
 
     @Override
     public void disconnect() {
-        if(!isValid()) return;
+        if (!isValid()) return;
 
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        source.close();
     }
 
     @Override
     public boolean isValid() {
-        if(connection == null) return false;
+        if (source == null) return false;
 
-        try {
-            return connection.isValid(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return false;
+        return source.isRunning();
     }
 }
